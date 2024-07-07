da3076b563b95b70752760e7bf996b9cd172485c
Edwin Varkey <146200949+Edwin-1234@users.noreply.github.com>
2024-07-07 13:15:43 +0300

    Create palindrome_cmdline.sh

---
+++ palindrome_cmdline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+num1=$1
+num2=$2
+echo "Palindrome numbers between $num1 and $num2"
+
+for (( i=$num1;i<=$num2;i++ ))
+do
+    rev=0
+    num=$i
+    while [ $num -gt 0 ]
+    do
+        digit=$(( $num % 10 ))
+        rev=$(( $rev* 10 + $digit ))
+        num=$(( $num / 10 ))
+    done
+    if [ $i -eq $rev ]
+    then
+        echo $i
+    fi
+done
