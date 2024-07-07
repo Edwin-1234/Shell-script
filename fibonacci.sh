dca3cc9ecb1ebc945f2b87ecdf00844ca7db34d6
Edwin Varkey <146200949+Edwin-1234@users.noreply.github.com>
2024-07-07 12:49:09 +0300

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "How many terms ? : "
+read n
+a=0
+b=1
+echo "Fibonacci Series for first $n terms"
+echo $a
+echo $b
+for (( i=2;i<=n;i++ ))
+do
+    c=$(($a+$b))
+    echo $c
+    a=$b
+    b=$c
+done
